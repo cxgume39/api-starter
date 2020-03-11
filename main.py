4158c4b74e4e099ec19c857472a8763ae130d541
Cornelio Gumecindo-Baltazar <cxgume39>
2020-03-11 08:03:41 -0400

    j
    mn

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 
-URL = "http://api.open-notify.org/astros.json"
+url = "https://privatix-temp-mail-v1.p.rapidapi.com/request/mail/id/%7Bmd5%7D/"
 
-res = requests.get(URL) # get the data
-res = res.json() # convert data to Python format
+headers = {
+    'x-rapidapi-host': "privatix-temp-mail-v1.p.rapidapi.com",
+    'x-rapidapi-key': "723a7f4069mshd39cc473ea7d918p1c8cacjsnd38cc7c107c8"
+    }
 
-print(res)
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
